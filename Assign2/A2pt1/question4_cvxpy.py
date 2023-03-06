ca34af51f951be4277f4cc68827d0e522f964841
Tanner Papenfuss <tjpapenfuss@gmail.com>
2023-03-06 12:10:05 -0500

    question 4 updated. It is not DGP so will not work

--- Assign2/A2pt1/question4_cvxpy.py
+++ Assign2/A2pt1/question4_cvxpy.py
@@ -5,24 +5,25 @@ import matplotlib.pyplot as plt
 x = cp.Variable(pos=True)
 y = cp.Variable(pos=True)
 
-a = cp.Parameter(nonpos=True)
+a = cp.Parameter(neg=True)
 b = cp.Parameter()
 
 objective_fn = (x**2 + y**2)
 objective = cp.Minimize(objective_fn)
-constraints = [y == (x + a)**(3/2)]
+constraints = [y == cp.power((x + a),(3/2))]
 #constraints = [a*(x*y + x*z + y*z) <= b, x >= y**c]
 problem = cp.Problem(objective, constraints)
 #plt.plot(objective_fn)
 print(np.pi)
-print(problem.is_dgp(dpp=True))
+print("problem is DCP:", problem.is_dcp())
+print("problem is DGP:", problem.is_dgp())
 
-a.value = -1.0
-b.value = 0.0
+# a.value = -1.0
+# b.value = 0.0
 
-problem.solve(gp=True)
-print("status:", problem.status)
-print("optimal value", problem.value)
+# problem.solve(gp=True)
+# print("status:", problem.status)
+# print("optimal value", problem.value)
 
-print(x.value)
-print(y.value)
+# print(x.value)
+# print(y.value)
