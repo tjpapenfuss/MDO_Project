793b5b7424535518d3d21fe83a405992d7559b8d
Tanner Papenfuss <tjpapenfuss@gmail.com>
2023-03-06 11:20:15 -0500

    Q2 is up and working.

--- Assign2/A2pt1/question2_cvxpy.py
+++ Assign2/A2pt1/question2_cvxpy.py
@@ -2,20 +2,26 @@ import cvxpy as cp
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = cp.Variable(nonneg=True)
-y = cp.Variable(nonneg=True)
+x = cp.Variable(pos=True)
+y = cp.Variable(pos=True)
 
+V = cp.Parameter(pos=True)
 
 objective_fn = ((2 * np.pi) * x * (x+y))
 objective = cp.Minimize(objective_fn)
-constraints = [(np.pi * (x**2) * y) - 1000 == 0, x>=0, y>=0]
+constraints = [(y == V/(np.pi*(x**2)))]
+#constraints = [((np.pi * (x**2) * y) - V == b)]
 #constraints = [a*(x*y + x*z + y*z) <= b, x >= y**c]
 problem = cp.Problem(objective, constraints)
 #plt.plot(objective_fn)
-print(np.pi)
-#print(problem.is_dgp(dpp=True))
+#print(np.pi)
+print("problem is DCP:", problem.is_dcp())
+print("problem is DGP:", problem.is_dgp())
 
-problem.solve(solver='GLPK_MI')
+#print(problem.is_dgp(dpp=True))
+V.value = 1000.0
+#b.value = 0.0
+problem.solve(gp=True)
 print("status:", problem.status)
 print("optimal value", problem.value)
 
