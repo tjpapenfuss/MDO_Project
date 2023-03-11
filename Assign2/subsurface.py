c22e5a2b7f1f6d4fb52c34b397e14aec50dab456
AndWarren <warren60@gmail.com>
2023-03-11 14:52:34 -0500

    I finished the wells library and created the module in Main

--- Assign2/subsurface.py
+++ Assign2/subsurface.py
@@ -5,8 +5,6 @@ import variables as v
 
 #Subsurface
 
-
-#make a def subsurface here - make sure that 
 def subsurface(p_wf_t):
     #variables
     p_wf_t =                2365.568 # Subsurface, wellbore injection pressure, PSI
@@ -20,14 +18,6 @@ def subsurface(p_wf_t):
     res_x = (0.703*v.perm*v.h_res*((p_wf_t**2)-(v.p_res**2)))
     fluid_y = (t_res_K*v.visc_CO2SC*v.Z_CO2SC*(Re_rw-.075+v.skin))
     
-
-    print("The Value of res_x is: " + str(res_x))
-    print("The Value of fluid_y is: " + str(fluid_y))
-    
     q_inj = res_x/fluid_y
 
-    print("The Value of q_inj is: " + str(q_inj))
     return q_inj
-
-
-
--- Assign2/wells.py
+++ Assign2/wells.py
@@ -1,38 +1,36 @@
 from numpy import *
-
 import variables as v
 
+#Wells
+def wells(Pwh):
+        
+        #these are the inputs that we will need from facilities when it is ready
+        avg_vol =               6 # Wells, Average Volocity of CO2 Coming down the pipe, ft/s
+        Pwh =                   1070 # Facilities, Wellhead pressure, PSI 
+        #variables
+        Pipe_id =               v.rw*2 # Wells, Inside Diameter of drill pipe, ft
+        mu_CO2sc =              v.visc_CO2SC*0.000672 # Wells, Viscosity of CO2 at SC, lbs/ft*s
+        g =                     32.147 # Wells, Gravitaional Constant, ft/s^2
+        
+        #Step 1 is to calculate the Reynolds Number Re in Centipoise    
+        Re = ((v.rho_CO2sc*avg_vol*Pipe_id)/mu_CO2sc)
+        #Note that the Re is diml
+        #print("The Value of the Reynolds Number is: " + str(Re))
 
-# wells
-Pipe_id =               v.rw*2 # Wells, Inside Diameter of drill pipe, ft
-n_wells =               0 # Wells,Number of wells,Number of injection wells in system
-avg_vol =               6 # Wells, Average Volocity of CO2 Coming down the pipe, ft/s
-mu_CO2sc =              v.visc_CO2SC*0.000672 # Wells, Viscosity of CO2 at SC, lbs/ft*s
-g =                     32.147 # Wells, Gravitaional Constant, ft/s^2
-Pwh =                   1070 # Wells, 
+        #Step 2 is to calculate the Fanning friction factor (function of Reynolds number)
+        fann = 0.0055*(1+(2*10**4*(v.rel_rough/Pipe_id)+(10**6/Re))**(1/3))
+        #Note that the fann is diml
+        #print("The Value of the Fanning Friction factor is: " + str(fann))
 
-#Step 1 is to calculate the Reynolds Number Re in Centipoise    
-        #for the actual model we will need to call the avg_vol from the facilities module
-Re = ((v.rho_CO2sc*avg_vol*Pipe_id)/mu_CO2sc)
-#Note that the Re is diml
-print("The Value of the Reynolds Number is: " + str(Re))
-#print(Re)
+        #Step 3 is to calculate the pressure loss due to friction effects
+        Delta_Pf = (((2*fann*v.rho_CO2sc*(avg_vol**2)*v.depth)/(g*Pipe_id))/144)
+        #Note that Delta_Pf units are PSIA (absolute PSI)
+        #Note that the equation is divided by 144 to conver to PSI
+        #print("The Value of Delta_Pf is: " + str(Delta_Pf))
 
-#Step 2 is to calculate the Fanning friction factor (function of Reynolds number)
-fann = 0.0055*(1+(2*10**4*(v.rel_rough/Pipe_id)+(10**6/Re))**(1/3))
-#Note that the fann is diml
-print("The Value of the Fanning Friction factor is: " + str(fann))
+        #Step 4 is to calculate the wellbore injection pressure
+                #for the actual model we will need to call the Pwh from the facilities module
+        p_wf_t = (Pwh-Delta_Pf+((v.rho_CO2sc*v.depth)/144))
+        #print("The Value of p_wf_t is: " + str(p_wf_t))
 
-#Step 3 is to calculate the pressure loss due to friction effects
-Delta_Pf = (((2*fann*v.rho_CO2sc*(avg_vol**2)*v.depth)/(g*Pipe_id))/144)
-#Note that Delta_Pf units are PSIA (absolute PSI)
-#Note that the equation is divided by 144 to conver to PSI
-print("The Value of Delta_Pf is: " + str(Delta_Pf))
-
-#Step 4 is to calculate the wellbore injection pressure
-        #for the actual model we will need to call the Pwh from the facilities module
-p_wf_t = (Pwh-Delta_Pf+((v.rho_CO2sc*v.depth)/144))
-print("The Value of p_wf_t is: " + str(p_wf_t))
-
-#Step 5 is to pass p_wf_t to the subsurface.py module
-
+        return p_wf_t
